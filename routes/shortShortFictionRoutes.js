c4de639c1b235d34ab1d2f7db056520adae84d22
Clay O'Dell <clayodell.swe@gmail.com>
2026-01-08 11:30:04 -0600

    updated poemRoutes

--- routes/shortShortFictionRoutes.js
+++ routes/shortShortFictionRoutes.js
@@ -1,7 +1,13 @@
+const express = require("express");
+const router = new express.Router();
+
 const ensureAdminLogin = require("../middleware/ensureAdminLogin");
+const ctrl = require("../controllers/shortShortFictionControllers");
 
 router.post("/draft", ensureAdminLogin, ctrl.createDraft);
 router.get("/", ensureAdminLogin, ctrl.list);
 router.patch("/:id", ensureAdminLogin, ctrl.updateDraft);
 router.post("/:id/publish", ensureAdminLogin, ctrl.publish);
 router.delete("/:id", ensureAdminLogin, ctrl.delete);
+
+module.exports = router;
